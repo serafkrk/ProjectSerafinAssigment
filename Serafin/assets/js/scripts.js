5de2615c9f9840ea8500706318f97cd1f0d39c2b
uverus2 <2ruzhk43@solent.ac.uk>
2019-06-30 23:40:36 +0100

    Done

---
+++ Serafin/assets/js/scripts.js
@@ -0,0 +1,6 @@
+function menuShow() {
+    var activeMenu = document.querySelector(".listenMenu");
+    var activeButton = document.querySelector(".listenButton");
+    activeMenu.classList.toggle("active");
+    activeButton.classList.toggle("active");
+ };
